98609d1629fc04637ea7a4ecd2f9a3d5a6e26778
Nurkybakyt <begaly606@gmail.com>
2023-09-26 23:22:09 +0600

    Initial Commit

---
+++ GitConnection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitConnection
+//
+//  Created by Dinmukhammed Begaly on 26.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
